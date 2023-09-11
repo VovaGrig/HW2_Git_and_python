8becf4c7855fefeec2149fc72958f3b8e8a91794
VovaGrig <vovagrig@DESKTOP-HRVLE2F>
2023-09-11 19:26:49 +0300

    Upgraded main function, deleted plus function

--- calculator.py
+++ calculator.py
@@ -1,17 +1,16 @@
 ### This will be our great calculator. Please contribute by writing one of the functions
-def plus(my_string): #numbers addition
-    ans = int(my_string.split(' ')[0]) + int(my_string.split(' ')[2])
-    return ans
 def main(): #accepts input, transmits to corresponding function and returns answer 
     my_string = str(input())
+    num1 = my_string.split(' ')[0]
+    num2 = my_string.split(' ')[2]
     if my_string.split(' ')[1] == "+":
-        ans = plus(my_string)
+        ans = plus(num1, num2)
     elif my_string.split(' ')[1] == "-":
-        ans = minus(my_string)
+        ans = minus(num1, num2)
     elif my_string.split(' ')[1] == "*":
-        ans = multiplication(my_string)
+        ans = multiplication(num1, num2)
     elif my_string.split(' ')[1] == "/":
-        ans = division(my_string)
+        ans = division(num1, num2)
     return ans
 answer = main()
 print(answer)
