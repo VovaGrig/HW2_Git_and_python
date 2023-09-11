a735b21819ddc1a13dea7f5b4b408395f19af6af
VovaGrig <vovagrig@DESKTOP-HRVLE2F>
2023-09-11 18:44:18 +0300

    Create main and plus functions

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
 ### This will be our great calculator. Please contribute by writing one of the functions
+def plus(my_string): 
+    ans = int(my_string.split(' ')[0]) + int(my_string.split(' ')[2])
+    return ans
+def main():
+    my_string = str(input())
+    if my_string.split(' ')[1] == "+":
+        ans = plus(my_string)
+    elif my_string.split(' ')[1] == "-":
+        ans = minus(my_string)
+    elif my_string.split(' ')[1] == "*":
+        ans = multiplication(my_string)
+    elif my_string.split(' ')[1] == "/":
+        ans = division(my_string)
+    return ans
+answer = main()
+print(answer)
