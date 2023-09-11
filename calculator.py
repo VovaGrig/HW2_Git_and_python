6499381047bf008af079ac61363156ca346a87d9
VovaGrig <vovagrig@DESKTOP-HRVLE2F>
2023-09-11 18:51:40 +0300

    Added comments to main and plus functions

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 ### This will be our great calculator. Please contribute by writing one of the functions
-def plus(my_string): 
+def plus(my_string): #numbers addition
     ans = int(my_string.split(' ')[0]) + int(my_string.split(' ')[2])
     return ans
-def main():
+def main(): #accepts input, transmits to corresponding function and returns answer 
     my_string = str(input())
     if my_string.split(' ')[1] == "+":
         ans = plus(my_string)
