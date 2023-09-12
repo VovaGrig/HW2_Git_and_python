11074e7f06ad7ef7d84dd12cdc69b73713e7edb9
VovaGrig <vovagrig@DESKTOP-HRVLE2F>
2023-09-12 20:07:31 +0300

    Create folder HW2_Grigoriants, moved calculator.py there

---
+++ HW2_Grigoriants/calculator.py
@@ -0,0 +1,16 @@
+### This will be our great calculator. Please contribute by writing one of the functions
+def main(): #accepts input, transmits to corresponding function and returns answer 
+    my_string = str(input())
+    num1 = my_string.split(' ')[0]
+    num2 = my_string.split(' ')[2]
+    if my_string.split(' ')[1] == "+":
+        ans = plus(num1, num2)
+    elif my_string.split(' ')[1] == "-":
+        ans = minus(num1, num2)
+    elif my_string.split(' ')[1] == "*":
+        ans = multiplication(num1, num2)
+    elif my_string.split(' ')[1] == "/":
+        ans = division(num1, num2)
+    return ans
+answer = main()
+print(answer)
